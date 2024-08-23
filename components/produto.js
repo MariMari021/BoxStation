fb6b964c651d2b5e7d5f7e55070cd7b4f77a2d3f
MariMari021 <133662908+MariMari021@users.noreply.github.com>
2024-08-23 10:09:15 -0300

    Add files via upload

--- components/produto.js
+++ components/produto.js
@@ -1,55 +1,177 @@
-import { Pressable, Text, StyleSheet, TouchableOpacity, View } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import { Pressable, Text, StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+import React, { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
 
 export function Produto({ data, onDelete, onSelect, selectedId }) {
     const isSelected = data.id === selectedId;
 
+    const [fontsLoaded] = useFonts({
+        'Fractul-Regular': require('../assets/fonts/Fractul-Regular.ttf'),
+        'Fractul-Bold': require('../assets/fonts/Fractul-Bold.ttf'),
+        'Fractul-SemiBold': require('../assets/fonts/Fractul-SemiBold.ttf'),
+        'Fractul-Light': require('../assets/fonts/Fractul-Light.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
     return (
         <Pressable
             style={[styles.container, isSelected ? styles.selectedContainer : null]}
             onPress={onSelect}
         >
-            <View>
-                
-                <Text style={styles.text}>
-                    NOME: <Text style={styles.valor}>{data.nome}</Text> 
-                </Text>
-                <Text style={styles.text}>
-                    QUANTIDADE: <Text style={styles.valor}>{data.quantidade}</Text>
-                </Text>
+            <View stytle={styles.containerDados} onLayout={onLayoutRootView}>
+                <View style={styles.alinhar}>
+                    <View style={styles.tudo}>
+                        <View style={styles.comeco}>
+                            <Image
+                                style={styles.detalhe}
+                                source={require('../assets/detalhe.png')}
+                            />
+                            <View style={styles.dados}>
+                                <View style={styles.infosContainer}>
+                                    <View style={styles.icone}>
+                                        <Image
+                                            style={styles.iconeLapis}
+                                            source={require('../assets/iconeLapis.png')}
+                                        />
+                                    </View>
+                                    <Text style={styles.nome}>
+                                        {data.nome}
+                                    </Text>
+                                </View>
+                                <View style={styles.infosContainer}>
+                                    <View style={styles.icone}>
+                                        <Image
+                                            style={styles.iconeNumber}
+                                            source={require('../assets/iconeNumber.png')}
+                                        />
+                                    </View>
+                                    <Text style={styles.nome}>
+                                        {data.quantidade}
+                                    </Text>
+                                </View>
+                            </View>
+                        </View>
+
+                    </View>
+                    <View style={styles.containerFinal}>
+                        <View style={styles.indice}>
+                            <Text style={styles.textoId}>{data.id}</Text>
+                        </View>
+                        <TouchableOpacity style={styles.botaoApagar} onPress={onDelete}>
+                            <Text style={styles.textoApagar}>Apagar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
-            <TouchableOpacity onPress={onDelete}>
-                <MaterialIcons name="delete" size={24} color="#000" />
-            </TouchableOpacity>
         </Pressable>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "#F5FFFA", // Mint cream color
-        padding: 16,
-        borderRadius: 12,
-        borderColor: '#4CAF50',
-        borderWidth: 1,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginVertical: 8,
+        backgroundColor: '#2E394D',
+        borderRadius: 20
+
     },
+
     selectedContainer: {
-        borderColor: '#2E7D32', // Darker green for selected state
-        borderWidth: 2,
+        borderWidth:5,
+        borderColor:'#8FB0EF',
+        shadowColor: '#000',  // Cor da sombra
+        shadowOffset: { width: 0, height: 2 },  // Deslocamento da sombra
+        shadowOpacity: 0.3,  // Opacidade da sombra
+        shadowRadius: 4,  // Raio da sombra (suavidade)
+        elevation: 5,  // Propriedade usada no Android para gerar sombra
     },
-    text: {
-        flex: 1,
-        color: '#333', // Dark text color
-        fontSize: 16,
-        fontWeight:'800',
-        color:'#649160'
+    alinhar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
-    valor:{
-        fontWeight:'500',
-        color:'#000'
+    tudo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 15,
+        paddingEnd: 20,
+        paddingStart: 20,
+
+    },
+    comeco: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    detalhe: {
+        width: 6.59,
+        height: 84,
+    },
+    dados: {
+        paddingStart: 10,
+        gap: 7
+    },
+    infosContainer: {
+        flexDirection: "row",
+        alignItems: 'center'
+    },
+    icone: {
+        backgroundColor: '#8FB0EF',
+        padding: 7,
+        borderRadius: 13,
+        marginEnd: 8
+    },
+    iconeLapis: {
+        width: 20,
+        height: 20
+    },
+    iconeNumber: {
+        width: 20,
+        height: 20
+    },
+    nome: {
+        fontFamily: 'Fractul-Light',
+        color: 'white',
+        fontSize: 17
+    },
+    containerFinal: {
+        alignItems: 'flex-end',
+        gap: 53
+
+    },
+    indice: {
+        padding: 3,
+        height: 29,
+        width: 28,
+        backgroundColor: '#8FB0EF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderTopRightRadius: 15,
+    },
+    textoId: {
+        fontFamily: "Fractul-Bold",
+        color: "white",
+        fontSize: 17
+    },
+    botaoApagar: {
+        backgroundColor: '#8FB0EF',
+        padding: 5,
+        height: 33,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 74,
+        borderBottomRightRadius: 15
+    },
+    textoApagar: {
+        fontFamily: "Fractul-SemiBold",
+        color: 'white',
+
     }
+
 });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Index.js
-import { View, StyleSheet, TextInput, Alert, FlatList, Pressable, TouchableOpacity, Text } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, TextInput, Alert, FlatList, Pressable, TouchableOpacity, Text, Image, StatusBar } from 'react-native';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
 import { usarBD } from './hooks/usarDB';
 import { Produto } from './components/produto';
 
@@ -23,7 +25,7 @@ export function Index() {
                 nome,
                 quantidade,
             });
-            Alert.alert('Produto cadastrado com o ID: ' + item.idProduto);
+            Alert.alert('Produto cadastrado com o Id: ' + item.idProduto);
             setId(item.idProduto);
             listar();
         } catch (error) {
@@ -76,78 +78,272 @@ export function Index() {
         }
     };
 
+    const [fontsLoaded] = useFonts({
+        'Fractul-Regular': require('./assets/fonts/Fractul-Regular.ttf'),
+        'Fractul-Bold': require('./assets/fonts/Fractul-Bold.ttf'),
+        'Fractul-SemiBold': require('./assets/fonts/Fractul-SemiBold.ttf'),
+        'Fractul-Light': require('./assets/fonts/Fractul-Light.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.inventario}>Inventário de Produtos</Text>
-                <TextInput
-                    style={[styles.texto, selectedId ? styles.selectedInput : null]}
-                    placeholder="Nome"
-                    onChangeText={setNome}
-                    value={nome}
-                />
-                <TextInput
-                    style={[styles.texto, selectedId ? styles.selectedInput : null]}
-                    placeholder="Quantidade"
-                    onChangeText={setQuantidade}
-                    value={quantidade}
-                />
-                <View style={styles.botao}>
-                    <TouchableOpacity style={styles.salvar} onPress={create}>
-                        <Text style={styles.textoSalvar}>Salvar</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.salvar} onPress={update} disabled={!selectedId}>
-                        <Text style={styles.textoSalvar}>Atualizar</Text>
+        <View style={styles.container} onLayout={onLayoutRootView}>
+            <StatusBar barStyle="light-content" backgroundColor="#8FB0EF" />
+            <View style={styles.logoContainer}>
+                <View style={styles.logo}>
+                    <Image
+                        style={styles.iconeLogo}
+                        source={require('./assets/logo.png')}
+                    />
+                    <Text style={styles.nomeEmpresa}>Box<Text style={styles.highlight}>Station</Text></Text>
+
+                </View>
+                <Text style={styles.tema}>Inventário</Text>
+            </View>
+            <View style={styles.marginbox}>
+                <View style={styles.box}>
+                    <Text style={styles.textoApresentacao}>Organize suas entregas de <Text style={styles.destaque}>E-commerce</Text>, aqui!.</Text>
+                    <View style={styles.inputContainer}>
+                        <View style={styles.icone}>
+                            <Image
+                                style={styles.iconeLapis}
+                                source={require('./assets/iconeLapis.png')}
+                            />
+                        </View>
+                        <TextInput
+                            style={[styles.input, selectedId ? styles.selectedInput : null]}
+                            placeholder="Nome da Mercadoria"
+                            onChangeText={setNome}
+                            value={nome}
+                        />
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <View style={styles.icone}>
+                            <Image
+                                style={styles.iconeNumber}
+                                source={require('./assets/iconeNumber.png')}
+                            />
+                        </View>
+                        <TextInput
+                            style={[styles.input, selectedId ? styles.selectedInput : null]}
+                            placeholder="Quantidade"
+                            onChangeText={setQuantidade}
+                            value={quantidade}
+                        />
+                    </View>
+                    <View style={styles.botao}>
+                        <TouchableOpacity style={styles.salvar} onPress={create}>
+                            <Text style={styles.textoSalvar}>Salvar!</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.atualizar} onPress={update} disabled={!selectedId}>
+                            <Text style={styles.textoAtualizar}>Atualizar</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+            </View>
+            <View style={styles.main}>
+                <View style={styles.search}>
+                    <TextInput
+                        style={styles.pesquisa}
+                        placeholder="Pesquisar"
+                        onChangeText={setPesquisa}
+                    />
+                    <TouchableOpacity style={styles.buscar}>
+                        <Text style={styles.textoBuscar}>Buscar</Text>
+                        <Image
+                            style={styles.iconeBuscar}
+                            source={require('./assets/iconeBuscar.png')}
+                        />
                     </TouchableOpacity>
                 </View>
-
+                <FlatList
+                    style={styles.flatList}
+                    contentContainerStyle={styles.listContent}
+                    data={produtos}
+                    keyExtractor={(item) => String(item.id)}
+                    renderItem={({ item }) => (
+                        <Produto
+                            data={item}
+                            onDelete={() => remove(item.id)}
+                            onSelect={() => handleSelect(item)}
+                            selectedId={selectedId}
+                        />
+                    )}
+                />
             </View>
-            <TextInput
-                style={styles.pesquisa}
-                placeholder="Pesquisar"
-                onChangeText={setPesquisa}
-            />
-            <FlatList
-                style={styles.flatList}
-                contentContainerStyle={styles.listContent}
-                data={produtos}
-                keyExtractor={(item) => String(item.id)}
-                renderItem={({ item }) => (
-                    <Produto
-                        data={item}
-                        onDelete={() => remove(item.id)}
-                        onSelect={() => handleSelect(item)}
-                        selectedId={selectedId}
-                    />
-                )}
-            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        paddingTop: '4%',
         flex: 1,
         justifyContent: 'center',
-        gap: 16,
-        backgroundColor: '#Fff',
+        backgroundColor: '#8FB0EF',
     },
-    header: {
-        paddingTop: 80,
-        padding: 32,
-        height: 350,
+    logoContainer: {
+        flexDirection: "row",
+        paddingStart: '8%',
+        paddingEnd: '8%',
+        alignItems: 'center',
+        justifyContent: "space-between",
+    },
+    logo: {
+        flexDirection: "row",
+        alignItems: 'center'
+    },
+    iconeLogo: {
+        width: 47,
+        height: 47
+    },
+    nomeEmpresa: {
+        fontFamily: 'Fractul-SemiBold',
+        color: "#fff",
+        fontSize: 21.7
+    },
+    highlight: {
+        color: "rgba(0, 0, 0, 0.68)"
+    },
+    tema: {
+        fontFamily: 'Fractul-Regular',
+        color: "white",
+        fontSize: 21
+    },
+    main: {
+        flex: 1,
+        backgroundColor: 'white',
+        borderTopEndRadius: 36,
+        borderTopStartRadius: 36,
+        paddingTop:65,
+        paddingStart:'8%',
+        paddingEnd:'8%'
+    },
+    marginbox: {
+        paddingStart: '8%',
+        paddingEnd: '8%',
+        position: 'relative',
+        top: 30,
+        zIndex: 1,
+    },
+    box: {
+        backgroundColor: '#2E394D',
+        paddingEnd: '10%',
+        paddingStart: '10%',
+        paddingTop: 27,
+        paddingBottom: 27,
+        borderRadius: 15,
         gap: 15,
-        borderBottomEndRadius: 25,
-        borderBottomStartRadius: 25,
-        backgroundColor: '#649160',
-        borderColor: "rgba(0, 0, 0, 0.1)",
-        borderWidth: 2
 
     },
+    textoApresentacao: {
+        fontFamily: 'Fractul-Regular',
+        color: "#fff",
+        fontSize: 17
+    },
+    destaque: {
+        fontFamily: 'Fractul-Bold'
+    },
+    inputContainer: {
+        flexDirection: "row",
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    icone: {
+        backgroundColor: '#8FB0EF',
+        padding: 10,
+        borderRadius: 20
+    },
+    iconeLapis: {
+        width: 24,
+        height: 24
+    },
+    iconeNumber: {
+        width: 24,
+        height: 24
+    },
+    input: {
+        height: 40,
+        backgroundColor: 'white',
+        width: 195,
+        paddingHorizontal: 18,
+        borderRadius: 7,
+    },
+    botao: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    salvar: {
+        backgroundColor: '#8FB0EF',
+        paddingVertical: 10,
+        width: 118,
+        alignItems: "center",
+        borderRadius: 18
+    },
+    textoSalvar: {
+        color: '#fff',
+        fontFamily: "Fractul-Bold",
+        fontSize: 15.7
+    },
+    atualizar: {
+        backgroundColor: '#fff',
+        paddingVertical: 10,
+        width: 118,
+        alignItems: "center",
+        borderRadius: 18
+    },
+    textoAtualizar: {
+        color: '#8FB0EF',
+        fontFamily: "Fractul-Bold",
+        fontSize: 15.7
+    },
+    search:{
+        backgroundColor:'rgba(217, 217, 217, 0.74)',
+        borderWidth:0.7,
+        borderColor:'rgba(46, 57, 77, 0.04)',
+        flexDirection:'row', 
+        justifyContent:"space-between",
+        paddingStart:35,
+        paddingEnd:7,
+        alignItems:"center",
+        height:45,
+        borderRadius:25,
+        marginBottom:32
+    },
+    buscar:{
+        flexDirection:"row",
+        alignItems:"center",
+        backgroundColor:'#Fff',
+        height:35,
+        borderRadius:18,
+        paddingStart:20,
+        paddingEnd:13,
+        justifyContent:'space-between'
+    },
+    iconeBuscar:{
+        width:27,
+        height:27
+
+    },
+    textoBuscar:{
+        fontFamily:'Fractul-SemiBold',
+        color:"#8FB0EF",
+        fontSize:16
+    },
     flatList: {
-        paddingStart: 32,
-        paddingEnd: 32,
-        marginBottom:42
+        paddingBottom: 52,
+        marginBottom:35,
     },
     texto: {
         height: 54,
@@ -157,40 +353,12 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         paddingHorizontal: 16,
     },
-    pesquisa: {
-        paddingVertical:10,
-        paddingHorizontal:15,
-        borderWidth: 1,
-        borderRadius: 7,
-        borderColor: "#999",
-        marginStart: 32,
-        marginEnd: 32,
-    },
     selectedInput: {
-        borderColor: '#4CAF50',
-        borderWidth: 2,
+        borderColor: '#8FB0EF',
+        borderWidth: 3,
     },
     listContent: {
         gap: 16,
     },
-    salvar:{
-        backgroundColor:'#fff',
-        paddingHorizontal:50,
-        paddingVertical:10,
-        borderRadius:10
-    },
-    botao:{
-        flexDirection:'row',
-        justifyContent:'space-between'
-    },
-    textoSalvar:{
-        color:'#649160',
-        fontWeight:'900',
-        fontSize:17
-    },
-    inventario:{
-        fontSize:25,
-        color:"#fff",
-        fontWeight:'900'
-    }
+
 });
